ea7ca48f279745eb47fe6f8cee13d38ba0447fa5
wei.shen <wei.shen@fanli.com>
2015-09-01 19:10:24 +0800

    login

--- src/main/java/com/fanli/scheduler/bean/interceptor/ApplicationLoginIntercepor.java
+++ src/main/java/com/fanli/scheduler/bean/interceptor/ApplicationLoginIntercepor.java
@@ -3,23 +3,37 @@ package com.fanli.scheduler.bean.interceptor;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by wei.shen on 2015/9/1.
  */
-public class ApplicationLoginIntercepor implements WebRequestInterceptor {
+public class ApplicationLoginIntercepor implements HandlerInterceptor {
+    private static final String LOGIN_URL = "/fanli/login";
+
     @Override
-    public void preHandle(WebRequest webRequest) throws Exception {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession httpSession = httpServletRequest.getSession(true);
+        Object obj = httpSession.getAttribute("user");
+        if (obj == null||"".equals(obj.toString())) {
+            httpServletResponse.sendRedirect(LOGIN_URL);
+            return false;
+        } else return true;
 
     }
 
     @Override
-    public void postHandle(WebRequest webRequest, ModelMap modelMap) throws Exception {
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
 
     }
 
     @Override
-    public void afterCompletion(WebRequest webRequest, Exception e) throws Exception {
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
 
     }
 }
---
+++ src/main/java/com/fanli/scheduler/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.fanli.scheduler.controller;
+
+import com.fanli.scheduler.bean.Result;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by wei.shen on 2015/9/1.
+ */
+@Controller
+public class LoginController {
+    private static Logger logger = Logger.getLogger(LoginController.class);
+    @RequestMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+}
